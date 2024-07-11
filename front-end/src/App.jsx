7d9b608f492d05b86d7e06069f1566d3d29424d4
LidyaGhilay <lidyahenok9@gmail.com>
2024-07-11 22:23:29 +0300

    Initial Commit

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,16 +1,21 @@
+// App.js
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home"; 
-import "./index.css"; 
+import Home from "./pages/Home";
+import About from "./pages/AboutUs";
+import "./index.css";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        
-      </Routes>
+      <div>
+        {/* Ensure Navbar is rendered only once */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
