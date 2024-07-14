b88d79b2a874b79ca54cbbbb12ca6b895fb789df
Peter <peterkanyi19193@gmail.com>
2024-07-14 12:32:15 +0300

    auth

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home"; 
+import Login from "./pages/Login"; 
+import Register from "./pages/Register";
+
 import "./index.css"; 
 
 function App() {
@@ -9,6 +12,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register/>} />
         
       </Routes>
     </Router>
