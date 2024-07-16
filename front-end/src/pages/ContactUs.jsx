92eee682362616674e22f7e5b763eb9e45dc4234
LidyaGhilay <lidyahenok9@gmail.com>
2024-07-16 11:21:20 +0300

    initial

--- front-end/src/pages/ContactUs.jsx
+++ front-end/src/pages/ContactUs.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-function ContactUs() {
-  return (
-    <div>ContactUs</div>
-  )
-}
+// ContactUs.js
 
-export default ContactUs
+import React from 'react';
+
+const ContactUs = () => {
+  return (
+    <div className="contact-us">
+      <h2>Contact Us</h2>
+      <p>If you have any questions or inquiries, please feel free to contact us using the information below:</p>
+      <div className="contact-info">
+        <p>Email: garizetu@yourcarcompany.com</p>
+        <p>Phone: 456-7890</p>
+        <p>Address: 123 Car Street, Cityville, State, Country</p>
+      </div>
+    </div>
+  );
+};
+
+export default ContactUs;
