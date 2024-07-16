d93461d7dcc9dcec94f57fc8209e02767a0982f0
Denis <mugambimuthuri0@gmail.com>
2024-07-16 19:57:17 +0300

    final commit on ft-lidya-backend

--- back-end/seed.py
+++ back-end/seed.py
@@ -51,7 +51,78 @@ catalogue5 = Catalogue(
     rating='4.9',
     release_date='2023-05-01'
 )
-
+catalogue6 = Catalogue(
+    image_url='https://s1.cdn.autoevolution.com/images/models/TOYOTA_Corolla--EU--2022_main.jpg',
+    brand='Toyota',
+    model='Corolla',
+    category='Sedan',
+    price='KES 2,500,000',
+    rating='4.5',
+    release_date='2022-11-15'
+)
+catalogue7 = Catalogue(
+    image_url='https://cgi.chevrolet.com/mmgprod-us/dynres/prove/image.gen?i=2024/CC10543/CC10543__1LT/G6M_09Y_0ST_1LT_1SZ_2ST_3ST_4AA_4ST_5FC_5ST_63G_6BJ_7BJ_8HB_9HB_A2X_A68_A7E_AED_AEQ_AKO_AL0_ASV_AU3_AVI_AVJ_AXG_AXK_AY0_AZ3_B1J_B30_B59_BTM_BTV_BWN_C49_C59_C5U_CGN_CJ2_CTT_D31_D75_DLF_DNS_DP9_E35_E63_EF7_ENL_F_FE9_FJW_G80_GF3_GU6_H0U_IOK_IVN_J24_J61_JBP_K34_KA1_KC9_KI3_KI4_KL9_KW5_L3B_MAH_MFC_MSL_N06_N37_N57_NB5_NTB_PCL_PDU_PED_PPW_PRF_QDF_QK2_QT5_RCV_RFQ_RIA_RM7_RSR_RWL_SAF_SLM_SU7_T8Z_TDM_TQ5_TUF_U2K_U73_UBI_UBJ_UDV_UE1_UE4_UEU_UF2_UH5_UHX_UHY_UJN_UK3_UKJ_UMN_UQF_URC_URD_UTJ_UVB_V46_V8D_VBJ_VJH_VK3_VRF_VRG_VRH_VRK_VRL_VRM_VRN_VRR_VSX_VT7_VV4_WLD_WMI_WMY_WPQ_X88_XCQ_YD9_YM8_Z82_Z85_ZL3_ZM9gmds2.jpg&v=deg01&std=true&country=US',
+    brand='Chevrolet',
+    model='Silverado',
+    category='Truck',
+    price='KES 5,000,000',
+    rating='4.6',
+    release_date='2023-02-28'
+)
+catalogue8 = Catalogue(
+    image_url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiS-YEzGlajBEMSNN2DpI6TbBFQ1gmaSvaKA&s',
+    brand='Nissan',
+    model='Altima',
+    category='Sedan',
+    price='KES 2,700,000',
+    rating='4.4',
+    release_date='2023-04-15'
+)
+catalogue9 = Catalogue(
+    image_url='https://imgd.aeplcdn.com/1280x720/n/cw/ec/150705/glc-exterior-right-front-three-quarter-94.jpeg?isig=0&q=80',
+    brand='Mercedes-Benz',
+    model='GLC',
+    category='SUV',
+    price='KES 6,200,000',
+    rating='4.9',
+    release_date='2023-06-20'
+)
+catalogue10 = Catalogue(
+    image_url='https://crdms.images.consumerreports.org/c_lfill,w_470,q_auto,f_auto/prod/cars/cr/model-years/15099-2023-audi-a4',
+    brand='Audi',
+    model='A4',
+    category='Sedan',
+    price='KES 4,500,000',
+    rating='4.7',
+    release_date='2023-07-05'
+)
+catalogue11 = Catalogue(
+    image_url='https://media.drive.com.au/obj/tx_q:70,rs:auto:1280:720:1/driveau/upload/vehicles/redbook/AUVSUBA2024AEAX/S000DEXK',
+    brand='Subaru',
+    model='Outback',
+    category='SUV',
+    price='KES 3,300,000',
+    rating='4.6',
+    release_date='2023-08-12'
+)
+catalogue12 = Catalogue(
+    image_url='https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_default/v1/editorial/vhs/Mitsubishi-Pajero.png',
+    brand='Mitsubishi',
+    model='Pajero',
+    category='SUV',
+    price='KES 4,000,000',
+    rating='4.5',
+    release_date='2023-09-25'
+)
+catalogue13 = Catalogue(
+    image_url='https://dashboard.kaiandkaro.com/media/vehicles/images/1000639682.jpg',
+    brand='Toyota',
+    model='Hiace',
+    category='Vans',
+    price='KES 3,299,999',
+    rating='4.5',
+    release_date='2016-09-25'
+)
 # Create instances for news blog
 news1 = News(
     image_url= 'https://i.ebayimg.com/images/g/b8wAAOSwE0JY~i3n/s-l1200.webp',
@@ -120,6 +191,14 @@ with app.app_context():
     db.session.add(catalogue3)
     db.session.add(catalogue4)
     db.session.add(catalogue5)
+    db.session.add(catalogue6)
+    db.session.add(catalogue7)
+    db.session.add(catalogue8)
+    db.session.add(catalogue9)
+    db.session.add(catalogue10)
+    db.session.add(catalogue11)
+    db.session.add(catalogue12)
+    db.session.add(catalogue13)
 
     db.session.add(news1)
     db.session.add(news2)
