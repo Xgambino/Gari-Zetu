84a5bfe09153b077c1357895740e07b68c4c7951
LidyaGhilay <lidyahenok9@gmail.com>
2024-07-16 11:29:03 +0300

    initial

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <Router>
       <div>
-        <Navbar /> 
+       
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<ContactUs />} /> 
--- front-end/src/pages/ContactUs.jsx
+++ front-end/src/pages/ContactUs.jsx
@@ -1,16 +1,33 @@
 // ContactUs.js
 
 import React from 'react';
+import '../ContactUs.css'; // Import CSS for ContactUs styling
 
 const ContactUs = () => {
   return (
     <div className="contact-us">
-      <h2>Contact Us</h2>
-      <p>If you have any questions or inquiries, please feel free to contact us using the information below:</p>
+      <div className="contact-header">
+        <h1>Contact Us</h1>
+        <p>If you have any questions or inquiries, please feel free to contact us using the information below:</p>
+      </div>
       <div className="contact-info">
-        <p>Email: garizetu@yourcarcompany.com</p>
-        <p>Phone: 456-7890</p>
-        <p>Address: 123 Car Street, Cityville, State, Country</p>
+        <div className="contact-details">
+          <h3>Contact Details</h3>
+          <p>Email: info@yourcarcompany.com</p>
+          <p>Phone: 123-456-7890</p>
+          <p>Address: 123 Car Street, Cityville, State, Country</p>
+        </div>
+        <div className="contact-map">
+          <h3>Our Location</h3>
+          <iframe
+            title="Company Location"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d23550.198731799675!2d-0.1276256351174794!3d51.50732132934852!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNTHCsDUyJzQwLjYiTiAwwrAyNic0Mi42Ilc!5e0!3m2!1sen!2suk!4v1588965902604!5m2!1sen!2suk"
+            frameBorder="0"
+            allowFullScreen=""
+            aria-hidden="false"
+            tabIndex="0"
+          ></iframe>
+        </div>
       </div>
     </div>
   );
