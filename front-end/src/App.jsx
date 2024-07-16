18a19603092ae11226e4f3edcc3a857619a04be2
LidyaGhilay <lidyahenok9@gmail.com>
2024-07-16 11:25:12 +0300

    initial

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -6,17 +6,21 @@ import "./index.css";
 import Catalogue from "./pages/Catalogue";
 import News from "./pages/News";
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs"; 
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contact" element={ContactUs} />
-        <Route path="/about" element={<AboutUs />} />
-        <Route path="/catalogue" element={<Catalogue />} />
-        <Route path="/news" element={<News />} />
-      </Routes>
+      <div>
+        <Navbar /> 
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<ContactUs />} /> 
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/catalogue" element={<Catalogue />} />
+          <Route path="/news" element={<News />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
